0b3ffd9dd6542862ab878db362ef71b7a26b5701
homer0 <leo.apiwan@olapic.com>
2018-06-03 05:51:49 -0300

    Add plugin code

---
+++ src/typedef.js
@@ -0,0 +1,22 @@
+/**
+ * @external {Projext}
+ * https://homer0.github.io/projext/class/src/app/index.js~Projext.html
+ */
+
+/**
+ * @external {Target}
+ * https://homer0.github.io/projext/typedef/index.html#static-typedef-Target
+ * @ignore
+ */
+
+/**
+ * @external {Targets}
+ * https://homer0.github.io/projext/class/src/services/targets/targets.js~Targets.html
+ * @ignore
+ */
+
+/**
+ * @external {BabelHelper}
+ * https://homer0.github.io/projext/class/src/services/common/babelHelper.js~BabelHelper.html
+ * @ignore
+ */
